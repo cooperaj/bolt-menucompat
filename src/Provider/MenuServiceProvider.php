efe1d5406b380e76b7466c9d7313f7eb94b3958b
Adam Cooper <adam@networkpie.co.uk>
2015-03-11 19:34:47 +0000

    Make MenuServiceProvider adhere to its contract and change the autoloading settings in composer.json

--- src/Provider/MenuServiceProvider.php
+++ src/Provider/MenuServiceProvider.php
@@ -7,8 +7,26 @@ use Silex\ServiceProviderInterface;
 
 class MenuServiceProvider implements ServiceProviderInterface
 {
-	public function register(Application $app)
-	{
+    /**
+     * Registers services on the given app.
+     *
+     * This method should only be used to configure services and parameters.
+     * It should not get services.
+     */
+    public function register(Application $app)
+    {
+        // TODO: Implement register() method.
+    }
 
-	}
+    /**
+     * Bootstraps the application.
+     *
+     * This method is called after all services are registered
+     * and should be used for "dynamic" configuration (whenever
+     * a service must be requested).
+     */
+    public function boot(Application $app)
+    {
+        // TODO: Implement boot() method.
+    }
 }
