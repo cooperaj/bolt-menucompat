606457428dd5ac7dc98678f348a38077a437d59e
Adam Cooper <adam@networkpie.co.uk>
2015-03-11 19:24:26 +0000

    Update of composer.json with new details. Creation of initial service provider to test how duplicated PSR4 namespaces work

---
+++ src/Provider/MenuServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bolt\Provider;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+class MenuServiceProvider implements ServiceProviderInterface
+{
+	public function register(Application $app)
+	{
+
+	}
+}
